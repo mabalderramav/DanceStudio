30c547c7aafe291af2540e8beadcfff2b11de0a3
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2025-11-20 19:10:33 -0400

    refactyor CreateSubscriptionCommand class

--- src/DanceStudio.Application/Subscriptions/Commands/CreateSubscription/CreateSubscriptionCommand.cs
+++ src/DanceStudio.Application/Subscriptions/Commands/CreateSubscription/CreateSubscriptionCommand.cs
@@ -7,5 +7,4 @@ namespace DanceStudio.Application.Subscriptions.Commands.CreateSubscription
     public record CreateSubscriptionCommand(
         SubscriptionType SubscriptionType,
         Guid AdminId) : IRequest<ErrorOr<Subscription>>;
-
 }
