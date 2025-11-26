9e7955b5acfe29e9048ef820eac9b9a9530a4bf9
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2025-11-26 18:48:42 -0400

    refactor helpers related to converter

--- src/DanceStudio.Infrastructure/Common/Persistence/ListOfIdsConverter.cs
+++ src/DanceStudio.Infrastructure/Common/Persistence/ListOfIdsConverter.cs
@@ -3,25 +3,12 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DanceStudio.Infrastructure.Common.Persistence
 {
-    public class ListOfIdsConverter : ValueConverter<List<Guid>, string>
-    {
-        public ListOfIdsConverter(ConverterMappingHints? mappingHints = null)
-            : base(
-                  x => string.Join(',', x),
-                  x => x.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(Guid.Parse).ToList(),
-                  mappingHints)
-        {
-        }
-    }
+    public class ListOfIdsConverter(ConverterMappingHints? mappingHints = null) : ValueConverter<List<Guid>, string>(
+        x => string.Join(',', x),
+        x => x.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(Guid.Parse).ToList(),
+        mappingHints);
 
-    public class ListOfIdsComparer : ValueComparer<List<Guid>>
-    {
-        public ListOfIdsComparer() : base(
-            (t1, t2) => t1!.SequenceEqual(t2!),
-            t => t.Select(x => x!.GetHashCode()).Aggregate((x, y) => x ^ y),
-            t => t
-            )
-        {
-        }
-    }
+    public class ListOfIdsComparer() : ValueComparer<List<Guid>>((t1, t2) => t1!.SequenceEqual(t2!),
+        t => t.Select(x => x.GetHashCode()).Aggregate((x, y) => x ^ y),
+        t => t);
 }
--- src/DanceStudio.Infrastructure/Common/Persistence/ValueJsonConverter.cs
+++ src/DanceStudio.Infrastructure/Common/Persistence/ValueJsonConverter.cs
@@ -4,25 +4,15 @@ using System.Text.Json;
 
 namespace DanceStudio.Infrastructure.Common.Persistence
 {
-    public class ValueJsonConverter<T> : ValueConverter<T, string>
-    {
-        public ValueJsonConverter(ConverterMappingHints? mappingHints = null)
-            : base(
-                  x => JsonSerializer.Serialize(x, JsonSerializerOptions.Default),
-                  x => JsonSerializer.Deserialize<T>(x, JsonSerializerOptions.Default)!,
-                  mappingHints
-                  )
-        {
-        }
-    }
+    public class ValueJsonConverter<T>(ConverterMappingHints? mappingHints = null) : ValueConverter<T, string>(
+        x => JsonSerializer.Serialize(x, JsonSerializerOptions.Default),
+        x => JsonSerializer.Deserialize<T>(x, JsonSerializerOptions.Default)!,
+        mappingHints);
 
-    public class ValueJsonComparer<T> : ValueComparer<T>
-    {
-        public ValueJsonComparer() : base(
-          (l, r) => JsonSerializer.Serialize(l, JsonSerializerOptions.Default) == JsonSerializer.Serialize(r, JsonSerializerOptions.Default),
-          v => v == null ? 0 : JsonSerializer.Serialize(v, JsonSerializerOptions.Default).GetHashCode(),
-          v => JsonSerializer.Deserialize<T>(JsonSerializer.Serialize(v, JsonSerializerOptions.Default), JsonSerializerOptions.Default)!)
-        {
-        }
-    }
+    public class ValueJsonComparer<T>() : ValueComparer<T>(
+        (l, r) => JsonSerializer.Serialize(l, JsonSerializerOptions.Default) ==
+                  JsonSerializer.Serialize(r, JsonSerializerOptions.Default),
+        v => v == null ? 0 : JsonSerializer.Serialize(v, JsonSerializerOptions.Default).GetHashCode(),
+        v => JsonSerializer.Deserialize<T>(JsonSerializer.Serialize(v, JsonSerializerOptions.Default),
+            JsonSerializerOptions.Default)!);
 }
