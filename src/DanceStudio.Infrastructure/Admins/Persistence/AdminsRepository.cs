0be8294ea0ab583cabc87540f33a82f373abe607
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2025-12-08 11:28:36 -0400

    refactor AdminsRepository.cs

--- src/DanceStudio.Infrastructure/Admins/Persistence/AdminsRepository.cs
+++ src/DanceStudio.Infrastructure/Admins/Persistence/AdminsRepository.cs
@@ -2,18 +2,11 @@
 using DanceStudio.Domain.Admins;
 using DanceStudio.Infrastructure.Common.Persistence;
 using Microsoft.EntityFrameworkCore;
-using System.Runtime.InteropServices;
 
 namespace DanceStudio.Infrastructure.Admins.Persistence
 {
-    public class AdminsRepository : IAdminsRepository
+    public class AdminsRepository(ApplicationDbContext context) : IAdminsRepository
     {
-        private readonly ApplicationDbContext context;
-
-        public AdminsRepository(ApplicationDbContext context)
-        {
-            this.context = context;
-        }
         public async Task<Admin?> GetByIdAsync(Guid adminId)
         {
             return await context.Admins.FirstOrDefaultAsync(x => x.Id == adminId);
